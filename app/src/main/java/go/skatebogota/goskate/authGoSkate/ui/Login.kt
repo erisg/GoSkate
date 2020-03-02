0ed8f9e54e1776e040157567b15a2ef2942b7cb8
erisbg <erisbg.513@gmail.com>
2020-03-01 22:17:29 -0500

    login user ui

--- app/src/main/java/go/skatebogota/goskate/authGoSkate/ui/Login.kt
+++ app/src/main/java/go/skatebogota/goskate/authGoSkate/ui/Login.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import go.skatebogota.goskate.R
 
-class login : AppCompatActivity() {
+class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/go/skatebogota/goskate/authGoSkate/ui/UserRegister.kt
@@ -0,0 +1,13 @@
+package go.skatebogota.goskate.authGoSkate.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import go.skatebogota.goskate.R
+
+class UserRegister : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+    }
+}
